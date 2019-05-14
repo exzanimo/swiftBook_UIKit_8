9bb7ac1286327939f9215d8abd6f176c76cb454d
Aleksandr Tarasov (exzanimo) <exzanimo@gmail.com>
2019-05-14 20:48:23 +0300

    Initial Commit

---
+++ UI_Course7 (WebKit)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Course7 (WebKit)
+//
+//  Created by Александр Тарасов on 14/05/2019.
+//  Copyright © 2019 Aleksandr Tarasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
